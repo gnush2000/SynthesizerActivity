6750724cbd51a97d840f1302cf16ccfa93e0aa64
Themi <Tcm4760@gmail.com>
2018-11-27 12:56:46 -0600

    push

--- app/src/main/java/com/example/isve1901/synthesizer/SynthesizerActivity.java
+++ app/src/main/java/com/example/isve1901/synthesizer/SynthesizerActivity.java
@@ -2,6 +2,8 @@ package com.example.isve1901.synthesizer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 public class SynthesizerActivity extends AppCompatActivity {
@@ -17,5 +19,15 @@ public class SynthesizerActivity extends AppCompatActivity {
 
         button1 = (Button)findViewById(R.id.button1);
         button2 = (Button)findViewById(R.id.button2);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.i(TAG, "Button 1 Clicked");
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.i(TAG, "Button 1 Clicked");
+            }
+        });
     }
 }
