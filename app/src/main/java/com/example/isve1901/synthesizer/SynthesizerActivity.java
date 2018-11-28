360fd9035163f8a5603adba2963358354a291046
gnush2000 <gnush2000@gmail.com>
2018-11-28 13:10:24 -0600

    f

--- app/src/main/java/com/example/isve1901/synthesizer/SynthesizerActivity.java
+++ app/src/main/java/com/example/isve1901/synthesizer/SynthesizerActivity.java
@@ -1,16 +1,20 @@
 package com.example.isve1901.synthesizer;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+
 public class SynthesizerActivity extends AppCompatActivity {
     private static final String TAG =
             SynthesizerActivity.class.getName();
     private Button button1;
     private Button button2;
+    private MediaPlayer mpE;
+    private MediaPlayer mpF;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +23,15 @@ public class SynthesizerActivity extends AppCompatActivity {
 
         button1 = (Button)findViewById(R.id.button1);
         button2 = (Button)findViewById(R.id.button2);
+
+        mpE = MediaPlayer.create(this, R.raw.scalee);
+        mpF = MediaPlayer.create(this, R.raw.scalef);
+
         button1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Log.i(TAG, "Button 1 Clicked");
+
+                mpE.start();
             }
         });
         button2.setOnClickListener(new View.OnClickListener() {
@@ -29,5 +39,6 @@ public class SynthesizerActivity extends AppCompatActivity {
                 Log.i(TAG, "Button 1 Clicked");
             }
         });
+
     }
 }
