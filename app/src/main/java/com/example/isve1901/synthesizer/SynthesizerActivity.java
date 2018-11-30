1248aed543efccb7812c5fe8bbe3b9d0102e8217
gnush2000 <gnush2000@gmail.com>
2018-11-30 12:44:59 -0600

    Buttons Added

--- app/src/main/java/com/example/isve1901/synthesizer/SynthesizerActivity.java
+++ app/src/main/java/com/example/isve1901/synthesizer/SynthesizerActivity.java
@@ -13,8 +13,14 @@ public class SynthesizerActivity extends AppCompatActivity {
             SynthesizerActivity.class.getName();
     private Button eButton;
     private Button mEButton;
+    private Button aButton;
+    private Button bButton;
     private MediaPlayer mpE;
     private MediaPlayer mpF;
+    private MediaPlayer mpA;
+    private MediaPlayer mpB;
+
+
     //this text does not matter
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +29,21 @@ public class SynthesizerActivity extends AppCompatActivity {
 
         eButton = (Button)findViewById(R.id.eButton);
         mEButton = (Button)findViewById(R.id.mEButton);
+        aButton = (Button)findViewById(R.id.aButton);
+        bButton = (Button)findViewById(R.id.bButton);
 
         mpE = MediaPlayer.create(this, R.raw.scalee);
         mpF = MediaPlayer.create(this, R.raw.scalef);
+        mpA = MediaPlayer.create(this, R.raw.scalea);
+        mpB = MediaPlayer.create(this, R.raw.scaleb);
 
-        mpE.seekTo(0);
+
 
         eButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Log.i(TAG, "Button 1 Clicked");
+                Log.i(TAG, "eButton Clicked");
 
+                mpE.seekTo(0);
                 mpE.start();
 
             }
@@ -40,10 +51,31 @@ public class SynthesizerActivity extends AppCompatActivity {
         mEButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 mpF.seekTo(0);
-                Log.i(TAG, "Button 1 Clicked");
+                Log.i(TAG, "mEButton Clicked");
+                mpF.start();
             }
         });
-        mpF.start();
+
+        aButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.i(TAG, "aButton Clicked");
+
+                mpA.seekTo(0);
+                mpA.start();
+
+            }
+        });
+
+        bButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.i(TAG, "bButton Clicked");
+
+                mpB.seekTo(0);
+                mpB.start();
+
+            }
+        });
+
 
     }
 }
